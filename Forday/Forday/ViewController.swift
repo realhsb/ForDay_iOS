cd7996490f5784426a77da5402534c40b5855ab8
realhsb <realhsb@gmail.com>
2026-01-05 01:40:03 +0900

    init a project

---
+++ Forday/Forday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Forday
+//
+//  Created by Subeen on 1/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
