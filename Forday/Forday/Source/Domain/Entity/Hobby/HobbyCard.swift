d6ad14b41cc2c55fe36673b63a1a8e15e55eade5
realhsb <realhsb@gmail.com>
2026-01-12 16:01:39 +0900

    feat: 취미 카드 dto, entity 정의

---
+++ Forday/Forday/Source/Domain/Entity/Hobby/HobbyCard.swift
@@ -0,0 +1,41 @@
+//
+//  HobbyCard.swift
+//  Forday
+//
+//  Created by Subeen on 1/12/26.
+//
+
+
+//
+//  HobbyCard.swift
+//  Forday
+//
+//  Created by Subeen on 1/12/26.
+//
+
+import UIKit
+
+struct HobbyCard {
+    let id: Int
+    let name: String
+    let description: String
+    let imageAsset: HobbyImageAsset
+}
+
+enum HobbyImageAsset: String {
+    case drawing = "drawing.png"
+    case gym = "gym.png"
+    case reading = "reading.png"
+    case music = "music.png"
+    case running = "running.png"
+    case cooking = "cooking.png"
+    case cafe = "cafe.png"
+    case movie = "movie.png"
+    case photo = "photo.png"
+    case writing = "writing.png"
+    
+    var image: UIImage? {
+        let assetName = self.rawValue.replacingOccurrences(of: ".png", with: "")
+        return UIImage(named: assetName)
+    }
+}
