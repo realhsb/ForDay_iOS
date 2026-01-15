f451308972540d41c74d1d1759862221ba617734
realhsb <realhsb@gmail.com>
2026-01-15 16:19:57 +0900

    chore: 취미 활동 AI mock

--- Forday/Forday/Source/Domain/Entity/AIRecommendation/AIRecommendation.swift
+++ Forday/Forday/Source/Domain/Entity/AIRecommendation/AIRecommendation.swift
@@ -21,3 +21,13 @@ struct AIRecommendation {
     let content: String
     let description: String
 }
+
+extension AIRecommendationResult {
+    static var stub01: AIRecommendationResult = .init(message: "test01", aiCallCount: 1, aiCallLimit: 3, activities: [.stub01, .stub02, .stub03])
+}
+
+extension AIRecommendation {
+    static var stub01: AIRecommendation = .init(activityId: 1, topic: "그림 그리기", content: "test content", description: "test description")
+    static var stub02: AIRecommendation = .init(activityId: 2, topic: "그림 그리기", content: "test content", description: "test description")
+    static var stub03: AIRecommendation = .init(activityId: 3, topic: "그림 그리기", content: "test content", description: "test description")
+}
