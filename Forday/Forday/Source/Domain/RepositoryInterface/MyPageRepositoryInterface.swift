8a9623a639c1bffeae2c1721ae286385b2cca10a
realhsb <realhsb@gmail.com>
2026-01-24 17:45:16 +0900

    feat: 마이페이지 repository

---
+++ Forday/Forday/Source/Domain/RepositoryInterface/MyPageRepositoryInterface.swift
@@ -0,0 +1,17 @@
+//
+//  MyPageRepositoryInterface.swift
+//  Forday
+//
+//  Created by Subeen on 1/23/26.
+//
+
+import Foundation
+
+protocol MyPageRepositoryInterface {
+    func fetchUserProfile() async throws -> UserProfile
+    func fetchMyActivities(hobbyId: Int?, page: Int, size: Int) async throws -> MyActivitiesResult
+    func fetchMyHobbies() async throws -> [MyPageHobby]
+    func fetchHobbyCards(page: Int) async throws -> [HobbyCardData]
+    func fetchActivityDetail(activityRecordId: Int) async throws -> ActivityDetail
+    func updateProfile(nickname: String?, profileImageUrl: String?) async throws -> UserProfile
+}
